947ceb45f9f619599df2222d5003164c0a77f6b7
milennik <nikola.milenkovic@outlook.com>
2023-03-26 01:30:36 +0100

    feat: add tokio and massping for ICMP.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 // use std::env::var;
 use std::thread::sleep;
-use std::time::Duration;
+use std::{net::IpAddr, sync::Arc, time::Duration};
 
+use futures_util::StreamExt;
+use massping::DualstackPinger;
+use std::net::ToSocketAddrs;
+use tokio::time;
 use tracing_subscriber::{layer::SubscriberExt, Registry};
 
 #[tracing::instrument(name = "fibonacci()")]
@@ -37,4 +41,42 @@ fn main() {
 
         fibonacci(4);
     });
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn pingg() {
+    println!("{:?}", ("google.com", 80).to_socket_addrs());
+    let google: IpAddr = "142.251.140.46".parse().unwrap();
+    let localhost_v4: IpAddr = "127.0.0.1".parse().unwrap();
+    let one_one_one_one_v4: IpAddr = "1.1.1.1".parse().unwrap();
+    let not_answering_v4: IpAddr = "0.0.0.1".parse().unwrap();
+    let localhost_v6: IpAddr = "::1".parse().unwrap();
+    let one_one_one_one_v6: IpAddr = "2606:4700:4700::1111".parse().unwrap();
+
+    let pinger = Arc::new(DualstackPinger::new().expect("setup pinger"));
+
+    let ips = [
+        google,
+        localhost_v4,
+        one_one_one_one_v4,
+        not_answering_v4,
+        localhost_v6,
+        one_one_one_one_v6,
+    ];
+
+    let mut interval = time::interval(Duration::from_secs(1));
+    loop {
+        interval.tick().await;
+
+        let pinger = Arc::clone(&pinger);
+        tokio::spawn(async move {
+            let _ = time::timeout(Duration::from_secs(5), async {
+                let mut stream = pinger.measure_many(ips.into_iter());
+                while let Some((addr, took)) = stream.next().await {
+                    println!("{}: {:?}", addr, took);
+                }
+            })
+            .await;
+        });
+    }
 }
